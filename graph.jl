cf1e3786990975643ab190acc79d78644588da3d
nathanafacion <nathanafacion@gmail.com>
2020-05-05 18:58:17 -0300

    Ajustes para funcionar para o corona Virus

--- graph.jl
+++
@@ -1,8 +0,0 @@
-f(x) = 3 * x^2 + 6 * x - 9
-for x = -5:5    
-  println("(",x, ", ", f(x), ")")
-end
-using Plots
-gr() # Activate the GR backend for use with Plots
-plot(f, -4, 3) # plot f over [-4,4]
-plot!(zero, -4, 3)
